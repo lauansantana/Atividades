029628034179d10148df3197d37f98dd81b32736
Lauan Santana <lauestora2@gmail.com>
2022-09-15 21:56:39 -0300

    Atividades

---
+++ Lista02Python/Q3.py
@@ -0,0 +1,17 @@
+#Questao3
+'''Faça um  algoritmo  que  leia  um nº inteiro  e mostre  uma  mensagem  indicando  se este número é par ou ímpar, e se é positivo ou negativo.'''
+
+
+numero = int(input('Informe um número: \n'))
+
+if numero%2==0 and numero>=0:
+    print('Número par positivo.')
+
+if numero%2==0 and numero<0:
+    print('Número par negativo.')
+
+if numero%2==1 and numero>=0:
+    print('Número ímpar positivo.')
+
+if numero%2==1 and numero<0:
+    print('Número ímpar negativo.')

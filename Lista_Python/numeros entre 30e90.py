ccaba39ad77dbee99cfd2a3288449b877b0a8cbd
Lauan Santana <lauestora2@gmail.com>
2022-09-28 20:54:35 -0300

    Atividades
    Listas de atividade da faculdade

--- Lista_Python/numeros entre 30e90.py
+++ Lista_Python/numeros entre 30e90.py
@@ -1,5 +1,5 @@
 #Questao 12
-#Faça um programa que recebe 10 númerose que calcule e mostre a quantidade dos números entre 30e 90.
+#Faça um programa que recebe 10 númerose que calcule e mostre a quantidade dos números entre 30 e 90.
 
 n = 0
 for numeros in range(0,10):
